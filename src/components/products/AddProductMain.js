ae2a1ad52ffc41830ecfe0709904d2d6d5f8d5ab
Elxan Quliyev <qelxan177@gmail.com>
2022-05-30 15:22:30 +0400

    first commit

---
+++ src/components/products/AddProductMain.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { PRODUCT_CREATE_RESET } from "../../Redux/Constants/ProductConstants";
+import { createProduct } from "./../../Redux/Actions/ProductActions";
+import Toast from "../LoadingError/Toast";
+import Message from "../LoadingError/Error";
+import Loading from "../LoadingError/Loading";
+
+const ToastObjects = {
+  pauseOnFocusLoss: false,
+  draggable: false,
+  pauseOnHover: false,
+  autoClose: 2000,
+};
+
+const AddProductMain = () => {
+  return (
+    <>
+      <Toast />
+      <section className="content-main" style={{ maxWidth: "1200px" }}>
+        <form onSubmit={submitHandler}>
+          <div className="content-header">
+            <Link to="/products" className="btn btn-danger text-white">
+              Go to products
+            </Link>
+            <h2 className="content-title">Add product</h2>
+            <div>
+              <button type="submit" className="btn btn-primary">
+                Publish now
+              </button>
+            </div>
+          </div>
+
+          <div className="row mb-4">
+            <div className="col-xl-8 col-lg-8">
+              <div className="card mb-4 shadow-sm">
+                <div className="card-body">
+                  {/* {error && <Message variant="alert-danger">{error}</Message>} */}
+                  {/* {loading && <Loading />} */}
+                  <div className="mb-4">
+                    <label htmlFor="product_title" className="form-label">
+                      Product title
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Type here"
+                      className="form-control"
+                      id="product_title"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="product_price" className="form-label">
+                      Price
+                    </label>
+                    <input
+                      type="number"
+                      placeholder="Type here"
+                      className="form-control"
+                      id="product_price"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="product_price" className="form-label">
+                      Count In Stock
+                    </label>
+                    <input
+                      type="number"
+                      placeholder="Type here"
+                      className="form-control"
+                      id="product_price"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Description</label>
+                    <textarea
+                      placeholder="Type here"
+                      className="form-control"
+                      rows="7"
+                      required
+                    ></textarea>
+                  </div>
+                  <div className="mb-4">
+                    <label className="form-label">Images</label>
+                    <input
+                      className="form-control"
+                      type="text"
+                      placeholder="Enter Image URL"
+                      required
+                    />
+                    <input className="form-control mt-3" type="file" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default AddProductMain;
---
+++ src/components/products/EditproductMain.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from "react";
+import Toast from "./../LoadingError/Toast";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  editProduct,
+  updateProduct,
+} from "./../../Redux/Actions/ProductActions";
+import { PRODUCT_UPDATE_RESET } from "../../Redux/Constants/ProductConstants";
+import { toast } from "react-toastify";
+import Message from "../LoadingError/Error";
+import Loading from "../LoadingError/Loading";
+
+const ToastObjects = {
+  pauseOnFocusLoss: false,
+  draggable: false,
+  pauseOnHover: false,
+  autoClose: 2000,
+};
+
+const EditProductMain = (props) => {
+  const { productId } = props;
+  return (
+    <>
+      <Toast />
+      <section className="content-main" style={{ maxWidth: "1200px" }}>
+        <form onSubmit={submitHandler}>
+          <div className="content-header">
+            <Link to="/products" className="btn btn-danger text-white">
+              Go to products
+            </Link>
+            <h2 className="content-title">Update Product</h2>
+            <div>
+              <button type="submit" className="btn btn-primary">
+                Publish now
+              </button>
+            </div>
+          </div>
+
+          <div className="row mb-4">
+            <div className="col-xl-8 col-lg-8">
+              <div className="card mb-4 shadow-sm">
+                <div className="card-body">
+                  {/* {errorUpdate && (
+                    <Message variant="alert-danger">{errorUpdate}</Message>
+                  )}
+                  {loadingUpdate && <Loading />}
+                  {loading ? (
+                    <Loading />
+                  ) : error ? (
+                    <Message variant="alert-danger">{error}</Message>
+                  ) : ( */}
+                    <>
+                      <div className="mb-4">
+                        <label htmlFor="product_title" className="form-label">
+                          Product title
+                        </label>
+                        <input
+                          type="text"
+                          placeholder="Type here"
+                          className="form-control"
+                          id="product_title"
+                          required
+                        />
+                      </div>
+                      <div className="mb-4">
+                        <label htmlFor="product_price" className="form-label">
+                          Price
+                        </label>
+                        <input
+                          type="number"
+                          placeholder="Type here"
+                          className="form-control"
+                          id="product_price"
+                          required
+                        />
+                      </div>
+                      <div className="mb-4">
+                        <label htmlFor="product_price" className="form-label">
+                          Count In Stock
+                        </label>
+                        <input
+                          type="number"
+                          placeholder="Type here"
+                          className="form-control"
+                          id="product_price"
+                          required
+                        />
+                      </div>
+                      <div className="mb-4">
+                        <label className="form-label">Description</label>
+                        <textarea
+                          placeholder="Type here"
+                          className="form-control"
+                          rows="7"
+                          required
+                        ></textarea>
+                      </div>
+                      <div className="mb-4">
+                        <label className="form-label">Images</label>
+                        <input
+                          className="form-control"
+                          type="text"
+                          required
+                        />
+                      </div>
+                    </>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default EditProductMain;
